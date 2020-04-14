471dfe553b457b88c6226ceef6bb7762cc94340f
Abdool Shakur <salamshakur@knights.ucf.edu>
2020-04-14 06:26:09 -0400

    login/signup page complete
    just needs programming logic such as api call

---
+++ app/app.js
@@ -0,0 +1,12 @@
+import Vue from 'nativescript-vue';
+
+import LoginPage from './components/LoginPage';
+
+// Uncommment the following to see NativeScript-Vue output logs
+// Vue.config.silent = false;
+
+new Vue({
+
+  render: h => h('frame', [h(LoginPage)])
+
+}).$start();
