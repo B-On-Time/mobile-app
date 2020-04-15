b5d5e67a4806c2c5aa88aa7bfb7b32b964630a74
Abdool Shakur <salamshakur@knights.ucf.edu>
2020-04-15 05:55:00 -0400

    punch buttons working and schedule section

--- app/components/Punch.vue
+++ app/components/Punch.vue
@@ -3,11 +3,19 @@
         <ActionBar title="Dashboard" />
         <ScrollView>
             <StackLayout class="dash-panel">
-                <Label text="Welcome" class="m-b-20" />
-                <Button text="Punch In!" @tap="onButtonTap"
-                    class="-primary -rounded-lg" />
-                <Button text="Punch Out!" @tap="onButtonTap"
-                    class="-primary -rounded-lg" isEnabled="false" />
+                <Label text="Welcome" class="welcome" />
+                <Button text="Punch In!" @tap="onPunchIn"
+                    class="-primary -rounded-lg" :isEnabled="punchToggle"/>
+                <Button text="Punch Out!" @tap="onPunchOut"
+                    class="-primary -rounded-lg" :isEnabled="!punchToggle" />
+                <Label text="My Schedule" class="schedule" />
+                <ListView class="list-group" for="schedule in schedules" style="height:1000px">
+                    <v-template>
+                        <FlexboxLayout flexDirection="row" class="list-group-item">
+                            <Label text="schedule examples..." style="width: 60%" />
+                        </FlexboxLayout>
+                    </v-template>
+                </ListView>
             </StackLayout>
         </ScrollView>
     </Page>
@@ -16,13 +24,22 @@
 <script>
     export default {
         methods: {
-            onButtonTap() {
-                console.log("Button was pressed");
+            onPunchIn() {
+                this.punchToggle = !this.punchToggle
+                console.log("You have punched in");
+            },
+
+            onPunchOut() {
+                this.punchToggle = !this.punchToggle
+                console.log("You have punched out");
             }
         },
         
         data() {
-            return {};
+            return {
+                punchToggle: true,
+                schedules: [1,2,3,4,5,6,7,8,9,10]
+            };
         }
     };
 </script>
@@ -32,5 +49,22 @@
         /* vertical-align: center; */
         font-size: 20;
         margin: 35;
+    }
+    .welcome {
+        horizontal-align: center;
+        font-size: 25;
+        font-weight: 600;
+        margin-bottom: 45;
+        text-align: center;
+        color: white;
+    }
+    .schedule {
+        horizontal-align: center;
+        font-size: 25;
+        font-weight: 600;
+        margin-top: 45;
+        margin-bottom: 45;
+        text-align: center;
+        color: white;
     }
 </style>
